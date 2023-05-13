f156a0f2c7131daf2f3df26cd466ccfa9613133f
hunputer <wlgns3855@gmail.com>
2023-05-13 13:06:03 +0900

    commit

--- src/main/java/com/example/chatting/Controller/ChatController.java
+++ src/main/java/com/example/chatting/Controller/ChatController.java
@@ -1,10 +1,13 @@
 package com.example.chatting.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.net.Socket;
@@ -13,29 +16,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@ServerEndpoint("/chat")
+@ServerEndpoint("/chat/{userId}")
 public class ChatController extends Socket {
     private static final List<Session> session = new ArrayList<Session>();
 
+    @GetMapping("/chatroom")
+    public String index(Model model){
+        return "chatroom";
+    }
+
     @OnOpen
-    public void open(Session newUser){
+    public void open(Session newUser, @PathParam(value="userId") String userId){
         System.out.println("connected");
         session.add(newUser);
         System.out.println(newUser.getId());
+        System.out.println(userId);
+        for(int i = 0; i< session.size(); i++){
+            try{
+                session.get(i).getBasicRemote().sendText(userId + "님이 입장하였습니다");
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @OnMessage
-    public void getMsg(Session recieveSession, String msg){
+    public void getMsg(Session recieveSession, String msg, @PathParam(value="userId") String userId){
         for(int i = 0; i< session.size(); i++){
             if(!recieveSession.getId().equals(session.get(i).getId())){
                 try{
-                    session.get(i).getBasicRemote().sendText("상대 : "+msg);
+                    session.get(i).getBasicRemote().sendText(userId +" : "+msg);
                 }catch (IOException e){
                     e.printStackTrace();
                 }
             }else{
                 try{
-                    session.get(i).getBasicRemote().sendText("나 : "+msg);
+                    session.get(i).getBasicRemote().sendText(userId +" : "+msg);
                 }catch(IOException e){
                     e.printStackTrace();
                 }
--- src/main/java/com/example/chatting/Controller/MainController.java
+++ src/main/java/com/example/chatting/Controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import javax.websocket.Session;
+
 @Controller
 public class MainController {
 
